ec83d8fa2aafd0ef67fa46e40915eb919f01e199
Bryce Payne <bryce.payne.97@gmail.com>
2022-07-12 18:27:45 -0500

    continued setting up the navbar

--- 5thGenShop/src/components/Products/Navbar/Navbar.jsx
+++ 5thGenShop/src/components/Products/Navbar/Navbar.jsx
@@ -5,16 +5,21 @@ import { ShoppingCart } from '@material-ui/icons';
 
 const Navbar = () => {
   return (
-    <div>
+    <>
        <AppBar position="fixed" className={classes.appBar} color='inherit'>
             <Toolbar>
                 <Typography variabnt="h6" className={classes.appBar} color="inherit">
                     <img src={} alt="5thGenShop" height="25px" className={classes.image} />
                     5thGenShop
                 </Typography>
+                <div className={classes.grow} />
+                <div className={classes.button}>
+                    <IconButton aria-label='Show Cart Items' color='inherit'></IconButton>
+                    
+                </div>
             </Toolbar>
         </AppBar>
-    </div>
+    </>
   )
 }
 
