00ca4edf7761076008a0e72f54753c23ec1c1c7e
Bryce Payne <bryce.payne.97@gmail.com>
2022-07-12 17:53:50 -0500

    figured out a name for site, changed file name

--- 5thGenShop/src/components/Products/Navbar/Navbar.jsx
+++ 5thGenShop/src/components/Products/Navbar/Navbar.jsx
@@ -9,8 +9,8 @@ const Navbar = () => {
        <AppBar position="fixed" className={classes.appBar} color='inherit'>
             <Toolbar>
                 <Typography variabnt="h6" className={classes.appBar} color="inherit">
-                    <img src={} alt="Commerce.js" height="25px" className={classes.image} />
-                    Commerce.js
+                    <img src={} alt="5thGenShop" height="25px" className={classes.image} />
+                    5thGenShop
                 </Typography>
             </Toolbar>
         </AppBar>
---
+++ 5thGenShop/src/components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import Product from './Product/Product';
+
+const products = [
+    {id: 1, name: 'Splitter', description: 'Front-Lip', price: '$1,000', image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+    {id: 2, name: 'SideSkirt', description: 'Side Part', price: '$1,000', image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+    {id: 3, name: 'Diffuser', description: 'Rear Car Part', price: '$1,000', image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+]
+
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify='center' spacing={4}>
+                {products.map(product => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>  
+                        <Product product={product}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+      );
+}
+ 
+export default Products;
